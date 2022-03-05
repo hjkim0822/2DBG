d20cc47c6d5c450f4666c7ecbcff7c755189ec2b
yoponchik <57009810+yoponchik@users.noreply.github.com>
2022-03-06 00:16:57 +0900

    Create ObstacleBouncetest.cs

---
+++ Assets/02. Scripts/Player/ObstacleBouncetest.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleBouncetest : MonoBehaviour
+{
+    public int bounceForce = 5;
+    private void OnCollisionEnter2D(Collision2D other) {
+        if (other.gameObject.CompareTag("Player")) {
+            print("Collision");
+            Rigidbody2D rb = other.gameObject.GetComponent<Rigidbody2D>();
+            Vector2 _playerMoveDirection = rb.velocity;
+            rb.AddForce(-_playerMoveDirection * bounceForce, ForceMode2D.Impulse);
+        }
+    }
+}
