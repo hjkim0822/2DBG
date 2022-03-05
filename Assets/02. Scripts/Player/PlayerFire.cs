5ba88583088d21073b2e1fa4d1964f1ca9e345c8
yoponchik <57009810+yoponchik@users.noreply.github.com>
2022-03-05 23:47:10 +0900

    Fire
    -PlayerFire.cs: Fire Rate
    -Delete Bullet.cs

--- Assets/02. Scripts/Player/PlayerFire.cs
+++ Assets/02. Scripts/Player/PlayerFire.cs
@@ -9,20 +9,37 @@ public class PlayerFire : MonoBehaviour
 
     public float bulletForce = 20f;
     public Transform firePosition;
+
+    public float fireRate = 2f;
+    bool canShoot = true;
     #endregion Declaration
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonDown("Fire1")){
+        if (Input.GetButton("Fire1")){
             Shoot();
         }
     }
 
     private void Shoot()
     {
-        GameObject bullet = Instantiate(bulletPrefab, firePosition.position, firePosition.rotation);
-        Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
-        rb.AddForce(bulletForce * firePosition.up, ForceMode2D.Impulse);
+        if (canShoot) { 
+            GameObject bullet = Instantiate(bulletPrefab, firePosition.position, firePosition.rotation);
+            Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+            rb.AddForce(bulletForce * firePosition.up, ForceMode2D.Impulse);
+
+            StopCoroutine(IEFireRate());
+            StartCoroutine(IEFireRate());
+        }
     }
+
+    IEnumerator IEFireRate() {
+        canShoot = false;
+        yield return new WaitForSeconds(fireRate);
+        canShoot = true;
+
+    }
+
+
 }
