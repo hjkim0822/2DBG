ab45ee73358008e07490557900886b4458055ae9
yoponchik <57009810+yoponchik@users.noreply.github.com>
2022-03-17 02:48:07 +0900

    Create PlayerNameInputField.cs

---
+++ Assets/02. Scripts/Player/Network/PlayerNameInputField.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine;
+
+
+[RequireComponent(typeof(TMP_InputField))]
+public class PlayerNameInputField : MonoBehaviour
+{
+    const string playerNamePrefKey = "PlayerName";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        #region Name Input
+        string defaultName = string.Empty;
+        TMP_InputField _inputField = this.GetComponent<TMP_InputField>();
+        if (_inputField != null) {
+            if (PlayerPrefs.HasKey(playerNamePrefKey)) {
+                defaultName = PlayerPrefs.GetString(playerNamePrefKey);
+                _inputField.text = defaultName;
+            }
+        }
+
+        PhotonNetwork.NickName = defaultName;
+        #endregion Name Input
+
+
+    }
+
+    #region Name Input: SetPlayerName()
+    public void SetPlayerName(string value) {
+        if (string.IsNullOrEmpty(value)) {
+            Debug.LogError("Player Name is null or empty");
+            return;
+        }
+        PhotonNetwork.NickName = value;
+        PlayerPrefs.SetString(playerNamePrefKey, value);
+    }
+    #endregion Name Input: SetPlayerName()
+
+
+}
